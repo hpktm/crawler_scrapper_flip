f2fe382b56958ebf78a94b6852ad054f92f121d4
hpktm <hitesh.pant94@gmail.com>
2019-02-11 08:06:00 +0530

    initial commit

--- get.py
+++ get.py
@@ -9,8 +9,6 @@ class scrapper:
     def __init__(self, baseurl):
         self.baseurl = baseurl
 
-# s=scrapper('https://www.flipkart.com')
-
 class crawler:
 	def __init__(self,baseurl,page):
 		self.baseurl=baseurl
@@ -20,7 +18,11 @@ class parent(scrapper,crawler):
     pass
 
     def scrap(self):
-    	print(self.baseurl)
+    	options=[];
+    	response=requests.get(self.baseurl);
+    	soup=BeautifulSoup(response.text,'html.parser')
+    	options=soup.find_all(class_="_3ybBIU")
+    	print(options)
 
 
 p=parent('https://www.flipkart.com');
